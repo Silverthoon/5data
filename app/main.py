36a76ee7f72cdce55d4c729f381a2ba3d8b6e0d5
emmanuel <manujellydev@gmail.com>
2021-12-21 10:28:01 +0100

    mise en place d'une nouvelle stack pyspark mongo et spark

--- app/main.py
+++ app/main.py
@@ -1,26 +1,31 @@
-from pyspark import SparkContext, SparkConf
-from pyspark.sql import SQLContext
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import col,date_format
+from pyspark.sql import SQLContext
 
-conf = SparkConf().set("spark.jars.packages", "org.mongodb.spark:mongo-spark-connector_2.12:3.0.1"). \
-    setMaster("spark://localhost:7077")
-
-sc = SparkContext(conf=conf)
-sqlContext = SQLContext(sc)
-
-spark = SparkSession.builder \
-    .appName("5DATA-PROJECT") \
+def init_spark():
+  sql = SparkSession.builder\
+    .appName("trip-app")\
+    .config("spark.jars.packages", "org.mongodb.spark:mongo-spark-connector_2.12:3.0.1")\
     .config("spark.mongodb.input.uri", "mongodb://root:example@localhost:27017/5data.students") \
     .config("spark.mongodb.output.uri", "mongodb://root:example@localhost:27017/5data.spark") \
     .getOrCreate()
+  sc = sql.sparkContext
+  return sql,sc
 
-df = spark.read.format("com.mongodb.spark.sql.DefaultSource").option("uri",
-                                                                     "mongodb://root:example@localhost:27017/5data"
-                                                                     ".students?authSource=admin").load()
-# df.printSchema()
+
+
+sql, sc = init_spark()
+sqlContext = SQLContext(sc)
+
+df = sql.read.format("com.mongodb.spark.sql.DefaultSource").option("uri","mongodb://root:example@localhost:27017/5data.students?authSource=admin").load()
+df.printSchema()
+
 
 
 df.createOrReplaceTempView("students")
 
-result_data = sqlContext.sql("SELECT COUNT(*), admission_year from students GROUP BY admission_year")
-result_data.show()
+result_data=sqlContext.sql("SELECT COUNT(*), admission_year from students GROUP BY admission_year")
+result_data.write.format("mongo")\
+  .option("uri","mongodb://root:example@localhost:27017/5data.spark?authSource=admin")\
+  .mode("append").save()
+
